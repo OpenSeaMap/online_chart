0322d42b992f2b8257156f99095b8fb87d8a007d
al_friede <al_friede@043453e6-25df-4c13-81b0-d6a77d00cc6f>
2009-08-17 16:17:30 +0000

    translated seamark and light for map key
    git-svn-id: https://openseamap.svn.sourceforge.net/svnroot/openseamap/website/map@24 043453e6-25df-4c13-81b0-d6a77d00cc6f

--- light.php
+++ light.php
@@ -1,27 +1,24 @@
 <table border="0" cellpadding="5">
-				<tr>
-					<td><IMG src="resources/light/Lighthouse_Major.png"  width="32" height="32" align="center" border="0"/></td>
-					<td>Leuchturm</td>
-
-				</tr>
-				<tr>
-					<td	height="5" class="normal" colspan="2">
-						<hr>
-					</td>
-				</tr>
-				<tr>
-					<td><IMG src="resources/light/Light_Beacon_Green.png" width="32" height="32" align="center" border="0"/></td>
-					<td>Molenfeuer grünes Licht</td>
-
-				</tr>
-				<tr>
-					<td><IMG src="resources/light/Light_Beacon_Red.png"  width="32" height="32" align="center" border="0"/></td>
-					<td>Molenfeuer rotes Licht</td>
-				</tr>
-				<tr>
-					<td><IMG src="resources/light/Light_Beacon_White.png"  width="32" height="32" align="center" border="0"/></td>
-					<td>Molenfeuer weißes Licht</td>
-
-				</tr>
-			</table>
+	<tr>
+		<td><IMG src="resources/light/Lighthouse_Major.png"  width="32" height="32" align="center" border="0"/></td>
+		<td><?=$t->tr("lighthouse")?></td>
+	</tr>
+	<tr>
+		<td	height="5" class="normal" colspan="2">
+			<hr>
+		</td>
+	</tr>
+	<tr>
+		<td><IMG src="resources/light/Light_Beacon_Green.png" width="32" height="32" align="center" border="0"/></td>
+		<td><?=$t->tr("beacon_green")?></td>
+	</tr>
+	<tr>
+		<td><IMG src="resources/light/Light_Beacon_Red.png"  width="32" height="32" align="center" border="0"/></td>
+		<td><?=$t->tr("beacon_red")?></td>
+	</tr>
+	<tr>
+		<td><IMG src="resources/light/Light_Beacon_White.png"  width="32" height="32" align="center" border="0"/></td>
+		<td><?=$t->tr("beacon_white")?></td>
+	</tr>
+</table>
 
--- seamark.php
+++ seamark.php
@@ -1,64 +1,57 @@
 <table border="0" cellpadding="5">
-				<tr>
-					<td><IMG src="resources/lateral/Lateral_SafeWater_Sphere.png" width="30" height="36" align="center" border="0"/></td>
-					<td>Ansteuerung</td>
-
-				</tr>
-				<tr>
-					<td><IMG src="resources/lateral/Lateral_Green_Conical.png" width="30" height="36" align="center" border="0"/></td>
-					<td>Fahrwassertonne Steuerbord</td>
-				</tr>
-				<tr>
-				<td><IMG src="resources/lateral/Lateral_Red_Cylindrical.png" width="30" height="36" align="center" border="0"/></td>
-					<td>Fahrwassertonne Backbord</td>
-
-				</tr>
-				<tr>
-					<td><IMG src="resources/lateral/Lateral_Pref_Starboard_Conical.png" width="30" height="36" align="center" border="0"/></td>
-					<td>Durchgehendes Fahrwasser: Steuerbord</td>
-				</tr>
-				<tr>
-					<td><IMG src="resources/lateral/Lateral_Pref_Port_Cylindrical.png" width="30" height="36" align="center" border="0"/></td>
-					<td>Durchgehendes Fahrwasser: Backbord</td>
-
-				</tr>
-				<tr>
-					<td	height="5" class="normal" colspan="2">
-						<hr>
-					</td>
-				</tr>
-				<tr>
-					<td><IMG src="resources/cardinal/Cardinal_North.png" width="30" height="36" align="center" border="0"/></td>
-					<td>Seezeichen nördlich der Gefahrenstelle</td>
-
-				</tr>
-				<tr>
-					<td><IMG src="resources/cardinal/Cardinal_East.png" width="30" height="36" align="center" border="0"/></td>
-					<td>Seezeichen östlich der Gefahrenstelle</td>
-				</tr>
-				<tr>
-					<td><IMG src="resources/cardinal/Cardinal_South.png" width="30" height="36" align="center" border="0"/></td>
-					<td>Seezeichen südlich der Gefahrenstelle</td>
-
-				</tr>
-				<tr>
-					<td><IMG src="resources/cardinal/Cardinal_West.png" width="30" height="36" align="center" border="0"/></td>
-					<td>Seezeichen westlich der Gefahrenstelle</td>
-				</tr>
-				<tr>
-					<td><IMG src="resources/cardinal/Cardinal_Single.png" width="30" height="36" align="center" border="0"/></td>
-					<td>Einzelgefahrenzeichen</td>
-
-				</tr>
-				<tr>
-					<td	height="5" class="normal" colspan="2">
-						<hr>
-					</td>
-				</tr>
-				<tr>
-					<td><IMG src="resources/special_purpose/Special_Purpose_x-Shape.png" width="30" height="36" align="center" border="0"/></td>
-					<td>Kennzeichnung besonderer Gebiete</td>
-
-				</tr>
-			</table>
+	<tr>
+		<td><IMG src="resources/lateral/Lateral_SafeWater_Sphere.png" width="30" height="36" align="center" border="0"/></td>
+		<td><?=$t->tr("safe_water")?></td>
+	</tr>
+	<tr>
+		<td><IMG src="resources/lateral/Lateral_Green_Conical.png" width="30" height="36" align="center" border="0"/></td>
+		<td><?=$t->tr("lateral_starboard")?></td>
+	</tr>
+	<tr>
+		<td><IMG src="resources/lateral/Lateral_Red_Cylindrical.png" width="30" height="36" align="center" border="0"/></td>
+		<td><?=$t->tr("lateral_port")?></td>
+	</tr>
+	<tr>
+		<td><IMG src="resources/lateral/Lateral_Pref_Starboard_Conical.png" width="30" height="36" align="center" border="0"/></td>
+		<td><?=$t->tr("lateral_pref_starboard")?></td>
+	</tr>
+	<tr>
+		<td><IMG src="resources/lateral/Lateral_Pref_Port_Cylindrical.png" width="30" height="36" align="center" border="0"/></td>
+		<td><?=$t->tr("lateral_pref_port")?></td>
+	</tr>
+	<tr>
+		<td	height="5" class="normal" colspan="2">
+			<hr>
+		</td>
+	</tr>
+	<tr>
+		<td><IMG src="resources/cardinal/Cardinal_North.png" width="30" height="36" align="center" border="0"/></td>
+		<td><?=$t->tr("cardinal_north")?></td>
+	</tr>
+	<tr>
+		<td><IMG src="resources/cardinal/Cardinal_East.png" width="30" height="36" align="center" border="0"/></td>
+		<td><?=$t->tr("cardinal_east")?></td>
+	</tr>
+	<tr>
+		<td><IMG src="resources/cardinal/Cardinal_South.png" width="30" height="36" align="center" border="0"/></td>
+		<td><?=$t->tr("cardinal_south")?></td>
+	</tr>
+	<tr>
+		<td><IMG src="resources/cardinal/Cardinal_West.png" width="30" height="36" align="center" border="0"/></td>
+		<td><?=$t->tr("cardinal_west")?></td>
+	</tr>
+	<tr>
+		<td><IMG src="resources/cardinal/Cardinal_Single.png" width="30" height="36" align="center" border="0"/></td>
+		<td><?=$t->tr("isolated_danger")?></td>
+	</tr>
+	<tr>
+		<td	height="5" class="normal" colspan="2">
+			<hr>
+		</td>
+	</tr>
+	<tr>
+		<td><IMG src="resources/special_purpose/Special_Purpose_x-Shape.png" width="30" height="36" align="center" border="0"/></td>
+		<td><?=$t->tr("special_purpose")?></td>
+	</tr>
+</table>
 
