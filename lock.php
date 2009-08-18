e96721ef46e0f58fe2b2b0559bfb6951dcbee0d5
al_friede <al_friede@043453e6-25df-4c13-81b0-d6a77d00cc6f>
2009-08-18 11:18:27 +0000

    updated translation
    git-svn-id: https://openseamap.svn.sourceforge.net/svnroot/openseamap/website/map@25 043453e6-25df-4c13-81b0-d6a77d00cc6f

--- lock.php
+++ lock.php
@@ -1,27 +1,24 @@
 <table border="0" cellpadding="5">
-				<tr>
-					<td><IMG src="resources/places/lock_gate.png"  width="18" height="32" align="center" border="0"/></td>
-					<td>Schleusentor</td>
-
-				</tr>
-				<tr>
-					<td><IMG src="resources/places/lock.png"  width="28" height="32" align="center" border="0"/></td>
-					<td>Schleuse</td>
-				</tr>
-				<tr>
-					<td	height="5" class="normal" colspan="2">
-						<hr>
-
-					</td>
-				</tr>
-				<tr>
-					<td><IMG src="resources/places/wier_small.png"  width="32" height="32" align="center" border="0"/></td>
-					<td>Wehr (klein)</td>
-				</tr>
-				<tr>
-					<td><IMG src="resources/places/wier_big.png"  width="32" height="32" align="center" border="0"/></td>
-
-					<td>Wehr (groß)</td>
-				</tr>
-			</table>
+	<tr>
+		<td><IMG src="resources/places/lock_gate.png"  width="18" height="32" align="center" border="0"/></td>
+		<td><?=$t->tr("lock_gate")?></td>
+	</tr>
+	<tr>
+		<td><IMG src="resources/places/lock.png"  width="28" height="32" align="center" border="0"/></td>
+		<td><?=$t->tr("lock")?></td>
+	</tr>
+	<tr>
+		<td	height="5" class="normal" colspan="2">
+			<hr>
+		</td>
+	</tr>
+	<tr>
+		<td><IMG src="resources/places/wier_small.png"  width="32" height="32" align="center" border="0"/></td>
+		<td><?=$t->tr("wier_small")?></td>
+	</tr>
+	<tr>
+		<td><IMG src="resources/places/wier_big.png"  width="32" height="32" align="center" border="0"/></td>
+		<td><?=$t->tr("wier_big")?></td>
+	</tr>
+</table>
 
