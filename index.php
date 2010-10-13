4e3b7aaa8a82d1199e6cd0b9b5b782884a52e251
al_friede <al_friede@043453e6-25df-4c13-81b0-d6a77d00cc6f>
2010-10-13 06:48:57 +0000

    added map download
    git-svn-id: https://openseamap.svn.sourceforge.net/svnroot/openseamap/website/map@289 043453e6-25df-4c13-81b0-d6a77d00cc6f

--- index.php
+++ index.php
@@ -20,6 +20,7 @@
 		<script type="text/javascript">
 
 			var map;
+			var arrayMaps = new Array();
 
 			// Position and zoomlevel of the map  (will be overriden with permalink parameters or cookies)
 			var lon = 11.6540;
@@ -28,6 +29,8 @@
 
 			// Work around for accessing translations from harbour.js
 			var linkText = "<?=$t->tr('descrSkipperGuide')?>";
+
+			var layer_download
 
 			// Load map for the first time
 			function init() {
@@ -53,6 +56,38 @@
  				legendWindow.focus();
 			}
 
+			// Show Download section
+			function showMapDownload() {
+				//alert("Download");
+				layer_download.setVisibility(true);
+				document.getElementById("downloadmenu").style.visibility = 'visible';
+			}
+
+			function closeMapDownload() {
+				layer_download.setVisibility(false);
+				document.getElementById("downloadmenu").style.visibility = 'hidden';
+			}
+
+			function downloadMap() {
+				var name = document.getElementById('info_dialog').innerHTML;
+				var format = _buoy_shape = document.getElementById("mapFormat").value;
+				if (format == "unknown") {
+					alert("Bitte wählen sie ein Format.");
+					return;
+				}
+				var url = "http://sourceforge.net/projects/openseamap/files/Maps/Europe/Baltic%20Sea/Harbour/"+ name + "/OSeaM-" + name + "." + format + "/download";
+				
+				downloadWindow = window.open(url);
+				//http://sourceforge.net/projects/openseamap/files/Maps/Europe/Baltic%20Sea/Harbour/StralsundHaven/OSeaM-StralsundHaven.WCI/download
+			}
+
+			function selectedMap (evt) {
+				var selectedMap = evt.feature.id.split(".");
+				var mapName = arrayMaps[selectedMap[2].split("_")[1]];
+				//alert(mapName);
+				document.getElementById('info_dialog').innerHTML=""+ mapName +"";
+			}
+
 			function drawmap() {
 				map = new OpenLayers.Map('map', {
 					projection: projMerc,
@@ -67,7 +102,7 @@
 						new OpenLayers.Control.ScaleLine({topOutUnits : "nmi", bottomOutUnits: "km", topInUnits: 'nmi', bottomInUnits: 'km', maxWidth: '40'}),
 						new OpenLayers.Control.LayerSwitcher(),
 						new OpenLayers.Control.MousePosition(),
-						 new OpenLayers.Control.OverviewMap(),
+						new OpenLayers.Control.OverviewMap(),
 						new OpenLayers.Control.PanZoomBar()],
 						maxExtent:
 						new OpenLayers.Bounds(-20037508.34, -20037508.34, 20037508.34, 20037508.34),
@@ -91,12 +126,20 @@
 				layer_harbours = new OpenLayers.Layer.Markers("<?=$t->tr("harbours")?>",
 				{ projection: new OpenLayers.Projection("EPSG:4326"), visibility: true, displayOutsideMaxExtent:true});
 				layer_harbours.setOpacity(0.8);
+				// Map download
+				layer_download = new OpenLayers.Layer.Vector("Map Download", {visibility: false});
 
-				map.addLayers([layer_mapnik, layer_tah, layer_sport, layer_seamark, layer_harbours]);
+				addDownloadlayer();
+
+				map.addLayers([layer_mapnik, layer_tah, layer_seamark, layer_harbours, layer_download, layer_sport]);
 
 				if (!map.getCenter()) {
 					jumpTo(lon, lat, zoom);
 				}
+				var selectDownload = new OpenLayers.Control.SelectFeature(layer_download);
+				map.addControl(selectDownload);
+				selectDownload.activate();
+				layer_download.events.register("featureselected", layer_download, selectedMap);
 			}
 
 			// Map event listener moved
@@ -114,11 +157,42 @@
 				setCookie("zoom", map.getZoom());
 			}
 
+			function addDownloadlayer() {
+
+				var xmlDoc=loadXMLDoc("./gml/map_download.xml");
+				try {
+					var root = xmlDoc.getElementsByTagName("maps")[0];
+					var items = root.getElementsByTagName("map");
+				} catch(e) {
+					alert("Error (root): "+ e);
+					return -1;
+				}
+				for (var i=0; i < items.length; ++i) {
+					//alert(i);
+					var item = items[i];
+					try {
+						var n = item.getElementsByTagName("north")[0].childNodes[0].nodeValue;
+						var s = item.getElementsByTagName("south")[0].childNodes[0].nodeValue;
+						var e = item.getElementsByTagName("east")[0].childNodes[0].nodeValue;
+						var w = item.getElementsByTagName("west")[0].childNodes[0].nodeValue;
+					} catch(e) {
+						alert("Error (load): "+ e);
+						return -1;
+					}
+					var bounds = new OpenLayers.Bounds(w, s, e, n);
+					bounds.transform(new OpenLayers.Projection("EPSG:4326"), new
+					OpenLayers.Projection("EPSG:900913"));
+					var box = new OpenLayers.Feature.Vector(bounds.toGeometry());
+					layer_download.addFeatures(box);
+					arrayMaps[box.id.split("_")[1]] = item.getElementsByTagName("name")[0].childNodes[0].nodeValue;
+				}
+			}
+
 		</script>
 	</head>
 	<body onload=init();>
-		<div id="map" style="position:absolute; bottom:0px; left:0px;">
-		</div>
+		<div id="map" style="position:absolute; bottom:0px; left:0px;"></div>
+		<div id="layerswitcher"></div>
 		<div style="position:absolute; bottom:48px; left:12px; width:700px;">
 			<img src="../resources/icons/somerights20.png" height="30px" title="<?=$t->tr("SomeRights")?>" onClick="window.open('http://creativecommons.org/licenses/by-sa/2.0')" />
 		</div>
@@ -129,7 +203,46 @@
 				<li onClick="window.location.href='./map_edit.php'"><IMG src="./resources/action/edit.png" width="24" height="24" align="center" border="0"><?=$t->tr("edit")?></img></li>
 				<li>&nbsp;|&nbsp;</li>
 				<li onClick="showMapKey()"><IMG src="./resources/action/info.png" width="24" height="24" align="center" border="0"><?=$t->tr("Legende")?></img></li>
+				<li>&nbsp;|&nbsp;</li>
+				<li onClick="showMapDownload()"><IMG src="./resources/action/download.png" width="24" height="24" align="center" border="0">Karte Herunterladen</img></li>
 			</ul>
+		</div>
+		<div id="downloadmenu" style="position:absolute; top:50px; left:60px; visibility:hidden;">
+			<b>Karte Herunterladen</b><br/><br/>
+			<table border="0" width="100%">
+				<tr>
+					<td>
+						Name:
+					</td>
+					<td>
+						<div id="info_dialog">&nbsp;Bitte Wählen<br/></div>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						Format:
+					<td>
+						<select id="mapFormat">
+							<option value="unknown"/><?=$t->tr("unknown")?>
+							<option value="png"/>png
+							<option value="kap"/>kap
+							<option value="WCI"/>WCI
+							<option value="kmz"/>kmz
+							<option value="jpr"/>jpr
+						</select>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<br/>
+						<input type="button" id="buttonMapDownload" value="Herunterladen" onclick="downloadMap()">
+					</td>
+					<td align="right">
+						<br/>
+						<input type="button" id="buttonMapClose" value="Schließen" onclick="closeMapDownload()">
+					</td>
+				</tr>
+			</table>
 		</div>
 	</body>
 </html>
