48da31ea5f9394c2c277b88bd7330f9f7ea676c5
al_friede <al_friede@043453e6-25df-4c13-81b0-d6a77d00cc6f>
2011-07-28 11:44:53 +0000

    updated to new version
    git-svn-id: https://openseamap.svn.sourceforge.net/svnroot/openseamap/website/map@423 043453e6-25df-4c13-81b0-d6a77d00cc6f

--- legend.php
+++ legend.php
@@ -3,10 +3,19 @@ include("../classes/Pages.php");
 include("../classes/Translation.php");
 
 $p = new Pages($_GET['page']);
+$p->addPage("divider1", "<b>".$t->tr("Legende")."</b>-----------------------","");
 $p->addPage("harbour",$t->tr("harbour"),"harbour.php");
 $p->addPage("seamark",$t->tr("Seezeichen"),"seamark.php");
 $p->addPage("light",$t->tr("Leuchtfeuer"),"light.php");
 $p->addPage("lock",$t->tr("BrückenSchleusen"),"lock.php");
+$p->addPage("divider2", "<b>".$t->tr("help")."</b>------------------------------","");
+$p->addPage("help-online-editor", $t->tr("help-oe"),"./map_key_pages/help-online-editor_".$t->tr("langCode").".php");
+$p->addPage("help-josm",$t->tr("help-josm"),"./map_key_pages/help-josm_".$t->tr("langCode").".php");
+$p->addPage("help-tidal-scale",$t->tr("help-tidal-scale"),"./map_key_pages/help-tidal-scale_".$t->tr("langCode").".php");
+$p->addPage("help-website",$t->tr("help-website-int"),"./map_key_pages/help-web-integr_".$t->tr("langCode").".php");
+$p->addPage("divider3", "------------------------------------","");
+$p->addPage("license",$t->tr("license"),"./map_key_pages/license_".$t->tr("langCode").".php");
+//$p->addPage("about",$t->tr("about"),"./map_key_pages/about_".$t->tr("langCode").".php");
 $p->setDefaultPage("harbour");
 
 ?>
@@ -15,11 +24,11 @@ $p->setDefaultPage("harbour");
        "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 	<head>
-	<title>OpenSeaMap: <?=$t->tr("Legende")?> - <?=$p->getCurrentPageName()?></title>
+	<title>OSW -<?=$t->tr('nautical_chart')?>: <?=$t->tr("Legende")?> - <?=$p->getCurrentPageName()?></title>
 		<meta name="AUTHOR" content="Olaf Hannemann" />
 		<meta http-equiv="content-type" content="text/html; charset=utf-8"/>
 		<meta http-equiv="content-language" content="<?= $t->getCurrentLanguage() ?>" />
-		<link rel="SHORTCUT ICON" href="../resources/icons/OpenSeaMapLogo_16.png"/>
+		<link rel="SHORTCUT ICON" href="../resources/icons/logo.png"/>
 		<link rel="stylesheet" type="text/css" href="map-legend.css">
 	</head>
 
@@ -28,11 +37,8 @@ $p->setDefaultPage("harbour");
 			<?=$p->makePageLinks("legend.php?lang=".$t->getCurrentLanguage()."&amp;")?>
 		</div>
 		<div id="content">
-			<br>
 			<?php include($p->makeIncludePage()); ?>
-			<br>
 		</div>
 	</body>
-
 </html>
 
