6723b4c18ab6dbcb0adbd6ebab21e431e778f409
al_friede <al_friede@043453e6-25df-4c13-81b0-d6a77d00cc6f>
2012-10-19 12:25:57 +0000

    Added Marine Traffic and OpenPortGuide
    git-svn-id: https://openseamap.svn.sourceforge.net/svnroot/openseamap/website/map@1029 043453e6-25df-4c13-81b0-d6a77d00cc6f

--- map_key_pages/license_de.php
+++ map_key_pages/license_de.php
@@ -1,6 +1,7 @@
+<!--updated 2012-06-02-->
 <table border="0" cellpadding="5">
 	<tr>
-		<td><IMG src="../resources/icons/somerights20.png" height="30" align="center" border="0"/></td>
+		<td><img alt="CC by SA" src="../resources/icons/somerights20.png" height="30" border="0"></td>
 		<td>Über die OpenSeaMap - Daten kann frei unter den Bedingungen der <a href="http://creativecommons.org/licenses/by-sa/2.0" target="_blank">Creative Commons Attribution-ShareAlike 2.0 Lizenz</a> verfügt werden.</td>
 	</tr>
 	<tr>
@@ -9,7 +10,25 @@
 		</td>
 	</tr>
 	<tr>
-  		<td><IMG src="../resources/icons/OSM-Logo-44px.png" height="44" align="center" border="0"/></td>
+  		<td><img alt="OSM-Logo" src="../resources/icons/OSM-Logo-44px.png" height="44" border="0"></td>
 		<td>Alle Daten der Basiskarte entstammen dem <a href="http://wiki.openstreetmap.org/wiki/DE:Main_Page" target="_blank">OpenStreetMap-Projekt</a>.</td>
+	</tr>
+	<tr>
+		<td	height="5" class="normal" colspan="2">
+			<hr>
+		</td>
+	</tr>
+	<tr>
+  		<td><img alt="OSM-Logo" src="../resources/icons/MarineTrafficLogo.png" height="38" border="0"></td>
+		<td>Alle AIS-Daten entstammen dem <a href="http://marinetraffic.com" target="_blank">Marine-Traffic-Projekt</a>.</td>
+	</tr>
+	<tr>
+		<td	height="5" class="normal" colspan="2">
+			<hr>
+		</td>
+	</tr>
+	<tr>
+  		<td><img alt="OSM-Logo" src="../resources/icons/OpenPortGuideLogo_32.png" height="44" border="0"></td>
+		<td>Alle Wetterinformationen entstammen dem <a href="http://openportguide.org/wiki_/Main_Page" target="_blank">OpenPortGuide-Projekt</a>.</td>
 	</tr>
 </table>
--- map_key_pages/license_en.php
+++ map_key_pages/license_en.php
@@ -1,6 +1,7 @@
+<!--updated 2012-06-02-->
 <table border="0" cellpadding="5">
 	<tr>
-		<td><IMG src="../resources/icons/somerights20.png" height="30" align="center" border="0"/></td>
+		<td><img alt="CC by SA" src="../resources/icons/somerights20.png" height="30" border="0"></td>
 		<td>OSM - Data can be used freely under the terms of the <a href="http://creativecommons.org/licenses/by-sa/2.0" target="_blank">Creative Commons Attribution-ShareAlike 2.0 license</a>.</td>
 	</tr>
 	<tr>
@@ -9,7 +10,25 @@
 		</td>
 	</tr>
 	<tr>
-  		<td><IMG src="../resources/icons/OSM-Logo-44px.png" height="44" align="center" border="0"/></td>
+  		<td><img alt="OSM-Logo" src="../resources/icons/OSM-Logo-44px.png" height="44" border="0"></td>
 		<td>All Data of the base map is created by the <a href="http://wiki.openstreetmap.org/wiki/DE:Main_Page" target="_blank">OpenStreetMap-Project</a>.</td>
+	</tr>
+	<tr>
+		<td	height="5" class="normal" colspan="2">
+			<hr>
+		</td>
+	</tr>
+	<tr>
+  		<td><img alt="OSM-Logo" src="../resources/icons/MarineTrafficLogo.png" height="38" border="0"></td>
+		<td>All AIS-Data is created by the <a href="http://marinetraffic.com" target="_blank">Marine-Traffic-Project</a>.</td>
+	</tr>
+	<tr>
+		<td	height="5" class="normal" colspan="2">
+			<hr>
+		</td>
+	</tr>
+	<tr>
+  		<td><img alt="OSM-Logo" src="../resources/icons/OpenPortGuideLogo_32.png" height="44" border="0"></td>
+		<td>All weather information is created by the <a href="http://openportguide.org/wiki_/Main_Page" target="_blank">OpenPortGuide-Project</a>.</td>
 	</tr>
 </table>
