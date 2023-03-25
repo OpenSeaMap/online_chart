bb502f34bf218db41d8c0743f17523ac5dc9b53b
Olivier Terral <olivier.terral@geops.com>
2023-03-25 10:59:24 +0100

    fix text

--- api/help/help-josm_en.php
+++ api/help/help-josm_en.php
@@ -1,6 +1,6 @@
 <iframe src="https://wiki.openseamap.org/wiki/JOSM_and_Plugin" width="540" height="670" frameborder="0">
 <p>Your browser can not show embedded frames:
-You may view the embedded site over the following
+You may view the embedded site through the following
 Call reference:
 https://wiki.openseamap.org/wiki/JOSM_and_Plugin
   </p>
